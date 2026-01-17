3b2c4feed6946b4130cf5287fa9d72d0a1d70dec
Jesse Cooke <jesse@ratiopbc.com>
2026-01-16 17:29:31 -0800

    Combine beneficiary summaries but add year

--- models/01_staging/claims/stg_beneficiary_summaries.sql
+++ models/01_staging/claims/stg_beneficiary_summaries.sql
@@ -1,4 +1,17 @@
 select
-  *
+  *,
+  2008 as YEAR
 from
-  {{ source('raw_claims', 'beneficiary_summaries') }}
+  {{ source('raw_claims', 'beneficiary_summaries_2008') }}
+union
+select
+  *,
+  2009 as YEAR
+from
+  {{ source('raw_claims', 'beneficiary_summaries_2009') }}
+union
+select
+  *,
+  2010 as YEAR
+from
+  {{ source('raw_claims', 'beneficiary_summaries_2010') }}
